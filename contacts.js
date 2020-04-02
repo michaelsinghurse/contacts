23970898070159475e0a3c7c7e86ac9bdc65bdc3
Michael Singhurse <michaelsinghurse@gmail.com>
2020-04-02 12:11:01 -0500

    added session persistence

--- contacts.js
+++ contacts.js
@@ -5,11 +5,14 @@
 const express = require('express');
 const morgan = require('morgan');
 const { body, validationResult } = require("express-validator");
+const session = require("express-session");
+const store = require("connect-loki");
 const PORT = 3000;
 
 const app = express();
+const LokiStore = store(session);
 
-let contactData = [
+const contactData = [
   {
     firstName: "Mike",
     lastName: "Jones",
@@ -48,6 +51,10 @@ const sortContacts = contacts => {
   });
 };
 
+function clone(object) {
+  return JSON.parse(JSON.stringify(object));
+}
+
 function containsOnlyLetters(str) {
   let regexp = /[^A-Za-z]/g;
   return !regexp.test(str);
@@ -82,8 +89,32 @@ app.set("views", "./views");  // note the relative folder path with `./`
 app.set("view engine", "pug");
 
 app.use(express.static("public"));
+
 app.use(express.urlencoded({ extended: false }));
+
 app.use(morgan("common"));
+
+app.use(session({
+  cookie: {
+    httpOnly: true,
+    maxAge: 31 * 24 * 60 * 60 * 1000, // 31 days in milliseconds
+    path: "/",
+    secure: false,
+  },
+  name: "launch-school-contacts-manager-session-id",
+  resave: false,
+  saveUninitialized: true,
+  secret: "this is not very secure",
+  store: new LokiStore({}),
+}));
+
+app.use((req, res, next) => {
+  if (!("contactData" in req.session)) {
+    req.session.contactData = clone(contactData);
+  }
+
+  next();
+});
 
 app.get("/", (req, res) => {
   res.redirect("/contacts");
@@ -91,7 +122,7 @@ app.get("/", (req, res) => {
 
 app.get("/contacts", (req, res) => {
   res.render("contacts", {
-    contacts: sortContacts(contactData),
+    contacts: req.session.contactData,
   });
 });
 
@@ -127,7 +158,7 @@ app.post("/contacts/new",
     }
   },
   (req, res) => {
-    contactData.push({ 
+    req.session.contactData.push({ 
       firstName: req.body.firstName,
       lastName: req.body.lastName,
       phoneNumber: req.body.phoneNumber,
